665ed8f1619409caada142211c943071582dbaeb
Joe Wilson <94833558+josephwilson8-nhs@users.noreply.github.com>
2023-11-21 18:50:33 +0000

    Adds failing tests for pytest exercise

--- main.py
+++ main.py
@@ -22,20 +22,20 @@ def example_add(num_1: int | float, num_2: int | float) -> int | float:
     return num_1 + num_2
 
 # TODO - Implement function and tests
-# def example_bad_add(num_1: int | float, num_2: int | float) -> int | float:
-#     """
-#     Badly adds two numbers together.
+def example_bad_add(num_1: int | float, num_2: int | float) -> int | float:
+    """
+    Badly adds two numbers together.
 
-#     Parameters
-#     ----------
-#     num_1 : int | float
-#         First number you want to add together.
-#     num_2 : int | float
-#         Second number you want to add together
+    Parameters
+    ----------
+    num_1 : int | float
+        First number you want to add together.
+    num_2 : int | float
+        Second number you want to add together
 
-#     Returns
-#     -------
-#     int | float
-#         Sum of num_1 and num_2... and + 1
-#     """
-#     return num_1 + num_2 + 1
+    Returns
+    -------
+    int | float
+        Sum of num_1 and num_2... and + 1
+    """
+    return num_1 + num_2 + 1
--- test_main.py
+++ test_main.py
@@ -25,19 +25,19 @@ def test_example_add(num_1, num_2, expected):
     assert actual==expected
 
 
-# @pytest.mark.parametrize(
-#     ["num_1", "num_2", "expected"],
-#     [
-#         (0, 0, 0),
-#         (1, 1, 2),
-#         (1, -1, 0),
-#         (0.1, 0.1, 0.2),
-#     ]
-# )
-# def test_example_bad_add(num_1, num_2, expected):
-#     """
-#     Tests main.example_bad_add
-#     """
-#     actual = main.example_bad_add(num_1, num_2)
+@pytest.mark.parametrize(
+    ["num_1", "num_2", "expected"],
+    [
+        (0, 0, 0),
+        (1, 1, 2),
+        (1, -1, 0),
+        (0.1, 0.1, 0.2),
+    ]
+)
+def test_example_bad_add(num_1, num_2, expected):
+    """
+    Tests main.example_bad_add
+    """
+    actual = main.example_bad_add(num_1, num_2)
 
-#     assert actual==expected
+    assert actual==expected
